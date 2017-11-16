9b032082fae85c3c742df4727c0e7267c17ff107
nataly <angynk17@gmail.com>
2017-11-15 21:53:40 -0500

    Ajustes Sugeridos

--- app/src/main/java/com/transmilenio/transmisurvey/activites/FrecRegistroActivity.java
+++ app/src/main/java/com/transmilenio/transmisurvey/activites/FrecRegistroActivity.java
@@ -1,13 +1,18 @@
 package com.transmilenio.transmisurvey.activites;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,7 +29,9 @@ import io.realm.Realm;
 public class FrecRegistroActivity extends AppCompatActivity {
 
 
-    private EditText horaPasoEditText, ocupacionEditText, codigoEditText;
+    private EditText  ocupacionEditText, codigoEditText;
+    private TextView horaPasoTextView;
+    private ImageButton horaButton;
     private FloatingActionButton buttonNuevo,buttonCerrar;
     private int  idEncuesta, idCuadro ;
     private FOcupacionEncuesta encuesta;
@@ -52,34 +59,38 @@ public class FrecRegistroActivity extends AppCompatActivity {
         if(extras != null){
             idEncuesta = (int) extras.get(ExtrasID.EXTRA_ID_ENCUESTA);
             idCuadro = (int) extras.get(ExtrasID.EXTRA_ID_CUADRO);
-//            servicio = (String) extras.get(ExtrasID.EXTRA_ID_SERVICIO);
             encuesta =  realm.where(FOcupacionEncuesta.class).equalTo("id",idCuadro).findFirst();
 
         }
     }
 
     private void bindUI() {
-        horaPasoEditText = (EditText) findViewById(R.id.fre_hora_paso_editText);
+//        horaPasoEditText = (EditText) findViewById(R.id.fre_hora_paso_editText);
+        horaPasoTextView = (TextView) findViewById(R.id.fre_hora_paso_editText);
         ocupacionEditText = (EditText) findViewById(R.id.fre_ocupacion_editText);
         codigoEditText = (EditText) findViewById(R.id.fre_codigo_editText);
         buttonNuevo = (FloatingActionButton) findViewById(R.id.fre_nuevoRegistro_button);
         buttonCerrar = (FloatingActionButton) findViewById(R.id.fre_cancelarRegistro_button);
+        horaButton = (ImageButton) findViewById(R.id.fre_getHora_button);
+        codigoEditText.setVisibility(View.INVISIBLE);
+        ocupacionEditText.setVisibility(View.INVISIBLE);
         agregarEventosBotones();
 
-      horaPasoEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (event != null && event.getAction() != KeyEvent.ACTION_DOWN) {
-                    return false;
-                } else if (actionId == EditorInfo.IME_ACTION_NEXT) {
-                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-                    String currentTime = sdf.format(new Date());
-                    horaPasoEditText.setText(currentTime);
-                    return false;
-                }
-                    return true;
-            }
-        });
+
+//      horaPasoEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                if (event != null && event.getAction() != KeyEvent.ACTION_DOWN) {
+//                    return false;
+//                } else if (actionId == EditorInfo.IME_ACTION_NEXT) {
+//                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+//                    String currentTime = sdf.format(new Date());
+//                    horaPasoEditText.setText(currentTime);
+//                    return false;
+//                }
+//                    return true;
+//            }
+//        });
 
     }
 
@@ -108,12 +119,27 @@ public class FrecRegistroActivity extends AppCompatActivity {
             }
         });
 
+        horaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                String currentTime = sdf.format(new Date());
+                horaPasoTextView.setText(currentTime);
+                codigoEditText.setVisibility(View.VISIBLE);
+                ocupacionEditText.setVisibility(View.VISIBLE);
+                codigoEditText.requestFocus();
+                codigoEditText.callOnClick();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(codigoEditText, InputMethodManager.SHOW_IMPLICIT);
+            }
+        });
+
     }
 
     private boolean agregarRegistro(){
         if(informacionCompletada()){
             final RegistroFrecOcupacion registro = new RegistroFrecOcupacion();
-            registro.setHora_paso(horaPasoEditText.getText().toString());
+            registro.setHora_paso(horaPasoTextView.getText().toString());
             registro.setServicio(codigoEditText.getText().toString());
             registro.setOcupacion(transformarValor(ocupacionEditText.getText().toString()));
             realm.executeTransaction(new Realm.Transaction() {
@@ -140,7 +166,7 @@ public class FrecRegistroActivity extends AppCompatActivity {
     private boolean informacionCompletada() {
         if( ocupacionEditText.getText().toString().trim().equals("") ||
                 codigoEditText.getText().toString().trim().equals("")  ||
-                horaPasoEditText.getText().toString().trim().equals("") ){
+                horaPasoTextView.getText().toString().trim().equals("") ){
             return false;
         }
         return true;
